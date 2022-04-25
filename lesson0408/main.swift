25eae7be765b4b4e653cfd6146139722b4e3a02c
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 <bbeka125@gmail.com>
2022-04-26 00:47:51 +0600

    добавил 3 возвращаемые функции

--- lesson0408/main.swift
+++ lesson0408/main.swift
@@ -8,44 +8,52 @@ print(contact1.change(new: 0500987654))
 
 
 
- let apples = 5
+let apples = 5
 let lemons = 10
 
 func addTwoNumbers(itemOne: Int, itemTwo:Int){
-    
-    let sum  = itemOne + itemTwo
-    
-    
- print(sum)
+   
+   let sum  = itemOne + itemTwo
+   
+   
+print(sum)
 }
 addTwoNumbers(itemOne: apples, itemTwo: lemons)
 
 func returnSumOfTwoNumbers(itemOne:Int,ItemTwo:Int) -> Int{
-    let sum = itemOne + ItemTwo
-    
-   return sum
-    
+   let sum = itemOne + ItemTwo
+   
+  return sum
+   
 }
 
 func sum(x:Int,y:Int) -> Int {
+   
+   let result = x + y
+   return result
+   
+}
+
+print(sum(x: 7, y: 5))
+
+
+
+func getEmail(Email:String) -> String{
     
-    let result = x + y
+    
+    let result = Email
+    
     return result
     
-}
-sum(x: 7, y: 5)
-
-
-
-
-
-func getEmail(Email: String) -> String {
     
-    
-   let result = Email
-    return result
-    getEmail(Email: "Hello")
 }
+
+
+
+print(getEmail(Email: "Hello"))
+
+
+
 
 
 
